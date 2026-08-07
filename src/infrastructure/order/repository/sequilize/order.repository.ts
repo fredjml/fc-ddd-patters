d94d386392404e9eb1acdb30d9f9ca3db6904f3d
Frederico José Monteiro Leite <fredjml.br@gmail.com>
2026-08-07 16:09:04 -0300

    first commit

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,4 +1,5 @@
 import Order from "../../../../domain/checkout/entity/order";
+import OrderItem from "../../../../domain/checkout/entity/order_item";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 
@@ -21,5 +22,22 @@ export default class OrderRepository {
         include: [{ model: OrderItemModel }],
       }
     );
+  }
+
+  async find(id: string): Promise<Order> {
+    const orderModel = await OrderModel.findOne({
+      where: { id },
+      include: [OrderItemModel],
+    });
+
+    if (!orderModel) {
+      throw new Error("Order not found");
+    }
+
+    const items = orderModel.items.map((item: any) => {
+      return new OrderItem(item.id, item.name, item.price, item.product_id, item.quantity);
+    });
+
+    return new Order(orderModel.id, orderModel.customer_id, items);
   }
 }
